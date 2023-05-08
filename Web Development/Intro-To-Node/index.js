599644742391a14e9fa439c18cddfa70f12a9398
Satyam Kumar <75772136+Satyam2003-dev@users.noreply.github.com>
2023-05-08 20:19:42 +0530

    Add files via upload

---
+++ Web Development/Intro-To-Node/index.js
@@ -0,0 +1,17 @@
+// jshint esversion:6
+
+// var superheroes = require('superheroes');
+
+// // var mysh = superheroes.all;
+
+// var mysh = superheroes.random();
+
+// console.log(mysh);
+
+var supervillains = require('supervillains');
+
+// var mysv = supervillains.all;
+
+var mysv = supervillains.random();
+
+console.log(mysv);
